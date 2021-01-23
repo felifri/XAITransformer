minor bug fixes with checkpoint saving and l2 conv computation

models.py
@@ -152,7 +152,7 @@ class ProtoPNetConv(ProtoPNet):
             x2_patch_sum = F.conv1d(input=x2, weight=self.ones[:n], dilation=d)
             xp = F.conv1d(input=x, weight=self.protolayer[j:j+n], dilation=d)
             # L2-distance aka sqrt(x² - 2xp + p²)
-            dist = torch.sqrt(torch.abs(x2_patch_sum - 2 * xp + p2_sum[j:j+n]))
+            dist = torch.sqrt(x2_patch_sum - 2 * xp + p2_sum[j:j+n])
             distances.append(dist)
             j += n
 

utils.py
@@ -9,11 +9,10 @@ import pickle
 
 # __import__("pdb").set_trace()
 
-def save_checkpoint(save_dir, state, time_stmp, best, filename='best_model.pth.tar'):
-    if best:
-        save_path_checkpoint = os.path.join(save_dir, time_stmp, filename)
-        os.makedirs(os.path.dirname(save_path_checkpoint), exist_ok=True)
-        torch.save(state, save_path_checkpoint)
+def save_checkpoint(save_dir, state, time_stmp, filename='best_model.pth.tar'):
+    save_path_checkpoint = os.path.join(save_dir, time_stmp, filename)
+    os.makedirs(os.path.dirname(save_path_checkpoint), exist_ok=True)
+    torch.save(state, save_path_checkpoint)
 
 def visualize_protos(embedding, labels, prototypes, n_components, trans_type, save_path):
         # visualize prototypes